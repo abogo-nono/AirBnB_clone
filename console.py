9b1fd251d315e720c4e3914ad119bbe2fb481e20
Abogo <abogonono1@gmail.com>
2024-05-22 03:58:12 +0100

    quit and EOF

--- console.py
+++ console.py
@@ -1,7 +1,20 @@
+import cmd
 from models.base_model import BaseModel
 
 
-class 
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
 
 
 # model = BaseModel()
